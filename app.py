fa4dafa68f5ac4c871dd3d0c3cf280db15f428e6
Opeoluwachosen <100340482+Opeoluwachosen@users.noreply.github.com>
2025-02-06 14:55:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 def is_prime(n):
     if n < 2:
         return False
-    for i in range(2, int(math.sqrt(n) + 1):
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
